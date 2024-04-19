d49451f57effbffed1f216fca57ae57e36d5f64e
ibrahim <ibra11002015@gmail.com>
2024-04-19 13:40:10 +0200

    created new function (changeBoardSize) to change the size of the board

--- script.js
+++ script.js
@@ -16,24 +16,12 @@ function draw(){
 
     generateBoard(8);
 
-    let button8 = document.querySelector('#s8');
-    let button16 = document.querySelector('#s16');
-    let button32 = document.querySelector('#s32');
-    let button64 = document.querySelector('#s64');
+    const button8 = document.querySelector('#s8');
+    const button16 = document.querySelector('#s16');
+    const button32 = document.querySelector('#s32');
+    const button64 = document.querySelector('#s64');
 
-    button8.addEventListener('click', function(e) {
-        let drawBoard = document.querySelector('.draw-board');
-        while(drawBoard.firstChild) {
-            drawBoard.firstChild.remove()
-        }
-        generateBoard(e.target.textContent);
-        const items = document.getElementsByClassName('item')
-        for (const item of items) {
-            item.addEventListener('mousedown', changeColor);
-        }
-    })
-
-    button16.addEventListener('click', function(e) {
+    function changeBoardSize(e) {
         const drawBoard = document.querySelector('.draw-board');
         while(drawBoard.firstChild) {
             drawBoard.firstChild.remove()
@@ -43,31 +31,12 @@ function draw(){
         for (const item of items) {
             item.addEventListener('mousedown', changeColor);
         }
-    })
-
-    button32.addEventListener('click', function(e) {
-        const drawBoard = document.querySelector('.draw-board');
-        while(drawBoard.firstChild) {
-            drawBoard.firstChild.remove()
-        }
-        generateBoard(e.target.textContent);
-        const items = document.getElementsByClassName('item')
-        for (const item of items) {
-            item.addEventListener('mousedown', changeColor);
-        }
-    })
-
-    button64.addEventListener('click', function(e) {
-        const drawBoard = document.querySelector('.draw-board');
-        while(drawBoard.firstChild) {
-            drawBoard.firstChild.remove()
-        }
-        generateBoard(e.target.textContent);
-        const items = document.getElementsByClassName('item')
-        for (const item of items) {
-            item.addEventListener('mousedown', changeColor);
-        }
-    })
+    }
+    
+    button8.addEventListener('click', changeBoardSize)
+    button16.addEventListener('click', changeBoardSize)
+    button32.addEventListener('click', changeBoardSize)
+    button64.addEventListener('click', changeBoardSize)
 
 
     const items = document.getElementsByClassName('item');
