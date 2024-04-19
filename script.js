b9811adaad1bace1ca3440eafb6cb0d195125c74
ibrahim <ibra11002015@gmail.com>
2024-04-19 08:25:59 +0200

    craeted the javascipt function responsible for changing the pixel color

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function draw(){
 
     function generateBoard(n) {
-        let drawBoard = document.querySelector('.draw-board');
+        const drawBoard = document.querySelector('.draw-board');
         for (let i = 0; i < n; i++) {
             let container = document.createElement('div');
             container.className = 'container';
@@ -22,10 +22,17 @@ function draw(){
     let button64 = document.querySelector('#64')
 
     button8.addEventListener('click', function(e) {
-        let drawBoard = document.querySelector('.draw-board');
+        const drawBoard = document.querySelector('.draw-board');
         while(drawBoard.firstChild) {
             drawBoard.firstChild.remove()
         }
-        generateBoard(e.target.textContent)
+        generateBoard(e.target.textContent);
+        const items = document.getElementsByClassName('item')
     })
+
+    function changeColor(e) {
+        const color = document.querySelector('#color');
+        e.target.style.backgrounColor = color.value;
+        e.target.style.outline = 'none';
+    }
 }
